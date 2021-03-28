b109cafbf6434954f6fb89664cfff1a3fd55a727
George Brotherston <George@StonePlayground.com>
2021-03-28 14:42:06 -0400

    Initial Commit
    Signed-off-by: George Brotherston <George@StonePlayground.com>

---
+++ src/Console/PublishCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace StonePlayground\Sail\Console;
+
+use Illuminate\Console\Command;
+
+class PublishCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sail:publish';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish the Laravel Sail Docker files';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->call('vendor:publish', ['--tag' => 'sail']);
+
+        file_put_contents(
+            $this->laravel->basePath('docker-compose.yml'), 
+            str_replace(
+                [
+                    './vendor/stoneplayground/sail/runtimes',
+                ],
+                [
+                    './docker',
+                ],
+                file_get_contents($this->laravel->basePath('docker-compose.yml'))
+            )
+        );
+    }
+}
